7fc76c0e4b7f452a7802999bbdae5797eb19e11a
BingBong <bingbong@pancakeswap.com>
2024-09-03 21:36:40 +0100

    first commit

---
+++ src/http/routes/base.route.ts
@@ -0,0 +1,40 @@
+import { Router } from "express";
+import type { Routes } from "../interfaces/routes.interface";
+
+class ValidatorRoute implements Routes {
+      public router: Router = Router();
+      public baseController: BaseController;
+      public readonly path = "/";
+
+      constructor() {
+            this.baseController = new BaseController(env);
+            this.initializeRoutes();
+      }
+
+      private initializeRoutes(): void {
+            this.router.get(`${this.path}/ping`, this.baseController.ping);
+            this.router.get(
+                  `${this.path}/pingServer/:port`,
+                  this.baseController.pingServer
+            );
+            this.router.get(`${this.path}/get/:key`, this.baseController.get);
+            this.router.get(
+                  `${this.path}/getAllKeys`,
+                  this.baseController.getAllKeys
+            );
+            this.router.get(
+                  `${this.path}/findNode/:nodeId`,
+                  this.baseController.findNode
+            );
+            this.router.get(
+                  `${this.path}/findValue/:key`,
+                  this.baseController.findValue
+            );
+            this.router.get(
+                  `${this.path}/store/:key/:value`,
+                  this.baseController.store
+            );
+      }
+}
+
+export default ValidatorRoute;
---
+++ src/utils/logger.ts
@@ -0,0 +1,42 @@
+import { createLogger, format, type Logger, transports } from "winston";
+import type * as Transport from "winston-transport";
+import config from "../config/config";
+
+export class AppLogger {
+      private logger: Logger;
+
+      constructor() {
+            this.initLogger();
+      }
+
+      private initLogger() {
+            if (!this.logger) {
+                  const transportsConfig: Transport[] = [new transports.Console()];
+                  const customFormat = format.combine(
+                        format.timestamp(),
+                        this.customPrintf()
+                  );
+
+                  this.logger = createLogger({
+                        format: customFormat,
+                        transports: transportsConfig,
+                        level: "debug",
+                  });
+            }
+      }
+
+      private customPrintf() {
+            return format.printf(({ level, message, label, timestamp }) => {
+                  return `${timestamp} | ${level
+                        .toLowerCase()
+                        .padEnd(5)} | ${label.padEnd(20)} | ${message}`;
+            });
+      }
+
+      getLogger(label: string) {
+            if (label.length > 30) {
+                  throw new Error("Too long label");
+            }
+            return this.logger.child({ label });
+      }
+}

64c87c28d64e5b91aa5b0519b0b0fb123790b7f8
mcgraneder <evanmcgranebb98@gmail.com>
2024-11-04 15:28:42 +0000

    feat: add peer

--- src/kBucket/kBucket.ts
+++ src/kBucket/kBucket.ts
@@ -3,9 +3,10 @@ import { MessagePayload, UDPDataInfo } from "../message/message";
 import { MessageType } from "../message/types";
 import { BIT_SIZE, K_BUCKET_SIZE } from "../node/constants";
 import KademliaNode from "../node/node";
+import { Peer } from "../peer/peer";
 
 export class KBucket {
-	public nodes: number[];
+	public nodes: Peer[];
 	public readonly bucketSize: number = BIT_SIZE;
 	public readonly parentNodeId: number;
 	public readonly bucketId: number;
@@ -19,16 +20,16 @@ export class KBucket {
 		this.node = node;
 	}
 
-	public getNodes(): Array<number> {
+	public getNodes(): Array<Peer> {
 		return this.nodes;
 	}
 
-	public removeNode = (nodeId: number) => {
-		this.nodes = this.nodes.filter((node: number) => node !== nodeId);
+	public removeNode = (node: Peer) => {
+		this.nodes = this.nodes.filter((peer: Peer) => peer.nodeId !== node.nodeId);
 	};
 
-	public async updateBucketNode(nodeId: number) {
-		const current = this.nodes.find((n) => n === nodeId);
+	public async updateBucketNode(peer: Peer) {
+		const current = this.nodes.find((node) => node.nodeId === peer.nodeId);
 
 		if (current) {
 			this.moveToFront(current);
@@ -36,30 +37,30 @@ export class KBucket {
 		}
 
 		if (this.nodes.length < K_BUCKET_SIZE) {
-			if (!this.nodes.includes(nodeId)) {
-			   this.nodes.push(nodeId);
+			if (!this.nodes.includes(peer)) {
+				this.nodes.push(peer);
 			}
 			return;
 		}
 		try {
 			// try check if node is only lone if not remove its id from the nodes arr
 			const recipient = {
-				address: (this.nodes[0] + 3000).toString(),
-				nodeId: this.nodes[0],
+				address: (this.nodes[0].nodeId + 3000).toString(),
+				nodeId: this.nodes[0].nodeId,
 			};
-			const payload = this.node.buildMessagePayload<UDPDataInfo>(MessageType.Ping, { resId: v4() }, this.nodes[0]);
+			const payload = this.node.buildMessagePayload<UDPDataInfo>(MessageType.Ping, { resId: v4() }, this.nodes[0].nodeId);
 			const message = this.node.createUdpMessage<UDPDataInfo>(recipient, MessageType.Ping, payload);
 			await this.node.udpTransport.sendMessage<MessagePayload<UDPDataInfo>>(message, this.node.udpMessageResolver);
 		} catch (e) {
 			this.nodes.shift();
-			if (!this.nodes.includes(nodeId)) {
-				this.nodes.push(nodeId);
+			if (!this.nodes.includes(peer)) {
+				this.nodes.push(peer);
 			}
 		}
 	}
 
-	public moveToFront(nodeId: number) {
-		this.nodes = [nodeId, ...this.nodes.filter((n) => n !== nodeId)];
+	public moveToFront(peer: Peer) {
+		this.nodes = [peer, ...this.nodes.filter((node: Peer) => node.nodeId !== peer.nodeId)];
 	}
 
 	toJSON() {
--- src/peer/peer.ts
+++ src/peer/peer.ts
@@ -1,18 +1,18 @@
 export class Peer {
-	public readonly nodeId: number;
-	public readonly address: string;
-	public readonly port: number;
+		public readonly nodeId: number;
+		// public readonly address: string;
+		// public readonly port: number;
 
-	public lastSeen: number;
+		// public lastSeen: number;
 
-	constructor(nodeId: number, address: string, port: number) {
-		this.nodeId = nodeId;
-		this.port = port;
-		this.address = address;
-		this.lastSeen = Date.now();
+		constructor(nodeId: number) {
+			this.nodeId = nodeId;
+			// this.port = port;
+			// this.address = address;
+			// this.lastSeen = Date.now();
+		}
+
+		// public updateLastSeen = () => {
+		// 	this.lastSeen = Date.now();
+		// };
 	}
-
-	public updateLastSeen = () => {
-		this.lastSeen = Date.now();
-	};
-}
